0644ce5155a40a933799e8887fe6c0edada75a9e
Alex Howells <alex@howells.me>
2012-10-09 06:38:04 +0200

    Bump version now python-cheetah is added

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Alex Howells"
 maintainer_email "alex@howells.me"
 license          "Apache 2.0"
 description      "Installs and configures sabnzbd onto a node"
-version          "1.1.0"
+version          "1.1.1"
  
 %w{ ubuntu }.each do |os|
   supports os
